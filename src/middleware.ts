93e9ca504c384e75c0734abbd0330977eaf473a4
_sa_tsuki <isobe@plusjam.jp>
2023-04-27 09:28:12 +0900

    basic認証解除

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,18 +5,15 @@ export const config = {
 };
 
 export function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get("authorization");
-  const url = req.nextUrl;
-
-  if (basicAuth) {
-    const authValue = basicAuth.split(" ")[1];
-    const [user, pwd] = atob(authValue).split(":");
-
-    if (user === process.env.NEXT_PUBLIC_USER && pwd === process.env.NEXT_PUBLIC_PASS) {
-      return NextResponse.next();
-    }
-  }
-  url.pathname = "/api/auth";
-
-  return NextResponse.rewrite(url);
+  // const basicAuth = req.headers.get("authorization");
+  // const url = req.nextUrl;
+  // if (basicAuth) {
+  //   const authValue = basicAuth.split(" ")[1];
+  //   const [user, pwd] = atob(authValue).split(":");
+  //   if (user === process.env.NEXT_PUBLIC_USER && pwd === process.env.NEXT_PUBLIC_PASS) {
+  //     return NextResponse.next();
+  //   }
+  // }
+  // url.pathname = "/api/auth";
+  // return NextResponse.rewrite(url);
 }
