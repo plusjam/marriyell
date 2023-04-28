9658b44bed623536adc1b603fc9f4e0913032bc1
_sa_tsuki <isobe@plusjam.jp>
2023-04-28 15:37:00 +0900

    meta追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,7 +18,21 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <GtmHead />
+        <meta
+          name="description"
+          content="京都 福知山の結婚式場・ウェディングなら“ル・クレア”。福知山駅徒歩3分でアクセス至便！北近畿最大級、天井高10mの白亜の大聖堂と、1件貸切の邸宅会場でゲストとともに心から楽しむウェディングを。"
+        />
+
+        {/* OG */}
         <meta property="og:image" content=" https://lu-crea.jp/ogp.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://lu-crea.jp/" />
+        <meta property="og:title" content="【公式】lu CREA ル・クレア┃京都 福知山の結婚式場" />
+        <meta property="og:site_name" content="lu CREA -ル・クレア- │京都 福知山の結婚式場" />
+        <meta
+          property="og:description"
+          content="京都 福知山の結婚式場・ウェディングなら“ル・クレア”。福知山駅徒歩3分でアクセス至便！北近畿最大級、天井高10mの白亜の大聖堂と、1件貸切の邸宅会場でゲストとともに心から楽しむウェディングを。"
+        ></meta>
       </Head>
 
       <AnimatePresence mode="wait" initial={false}>
