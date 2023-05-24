f31155072a16da8c6b97c3ce147ee6e4e431debd
_sa_tsuki <isobe@plusjam.jp>
2023-05-24 23:50:12 +0900

    input[type=time]をselectに変更

--- src/components/mols/DetailPlanFormInput.tsx
+++ src/components/mols/DetailPlanFormInput.tsx
@@ -210,44 +210,41 @@ const DetailPlanFormInput = (props: Props) => {
             </label>
             <div className={Styles.inputBlockWrap}>
               <div className={`${Styles.inputBlock} ${Styles.triangle}`}>
-                <input
-                  className={`${Styles.input} ${Styles.time}`}
-                  id="time"
+                <select
                   {...register("time", {
                     required: rules.required,
                     onChange: (e) => {
                       handleData({ ...data, time: e.target.value });
                     },
                   })}
-                  type="time"
-                  value={data.time}
-                  list="data-time"
-                />
-                <datalist id="data-time">
-                  <option value="09:00"></option>
-                  <option value="09:30"></option>
-                  <option value="10:00"></option>
-                  <option value="10:30"></option>
-                  <option value="11:00"></option>
-                  <option value="11:30"></option>
-                  <option value="12:00"></option>
-                  <option value="12:30"></option>
-                  <option value="13:00"></option>
-                  <option value="13:30"></option>
-                  <option value="14:00"></option>
-                  <option value="14:30"></option>
-                  <option value="15:00"></option>
-                  <option value="15:30"></option>
-                  <option value="16:00"></option>
-                  <option value="16:30"></option>
-                  <option value="17:00"></option>
-                  <option value="17:30"></option>
-                  <option value="18:00"></option>
-                  <option value="18:30"></option>
-                  <option value="19:00"></option>
-                  <option value="19:30"></option>
-                  <option value="20:00"></option>
-                </datalist>
+                  className={`${Styles.input} ${Styles.select}`}
+                >
+                  <option value="9:00" selected>
+                    9:00
+                  </option>
+                  <option value="9:30">9:30</option>
+                  <option value="10:00">10:00</option>
+                  <option value="10:30">10:30</option>
+                  <option value="11:00">11:00</option>
+                  <option value="11:30">11:30</option>
+                  <option value="12:00">12:00</option>
+                  <option value="12:30">12:30</option>
+                  <option value="13:00">13:00</option>
+                  <option value="13:30">13:30</option>
+                  <option value="14:00">14:00</option>
+                  <option value="14:30">14:30</option>
+                  <option value="15:00">15:00</option>
+                  <option value="15:30">15:30</option>
+                  <option value="16:00">16:00</option>
+                  <option value="16:30">16:30</option>
+                  <option value="17:00">17:00</option>
+                  <option value="17:30">17:30</option>
+                  <option value="18:00">18:00</option>
+                  <option value="18:30">18:30</option>
+                  <option value="19:00">19:00</option>
+                  <option value="19:30">19:30</option>
+                  <option value="20:00">20:00</option>
+                </select>
 
                 {errors.time && <span className={Styles.error}>{errors.time.message as string}</span>}
               </div>
