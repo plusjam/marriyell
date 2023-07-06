5c33f53a6c1ee477eeb832cf4918a7fbe5c3d135
sa-tsuki <isobe@plusjam.jp>
2023-07-06 19:40:25 +0900

    🐛

--- src/components/atoms/ContactPolicy.tsx
+++ src/components/atoms/ContactPolicy.tsx
@@ -5,7 +5,7 @@ import Styles from "@/styles/orgs/ContactForm.module.scss";
 const ContactPolicy = () => {
   return (
     <p className={Styles.policy}>
-      <Link href="https://mariage-collection.com/marriyell/privacy/" target="_blank" rel="noopener noreferrer" className={Styles.link}>
+      <Link href="https://marriyellclub.co.jp/company/privacypolicy.html" target="_blank" rel="noopener noreferrer" className={Styles.link}>
         プライバシーポリシー
       </Link>
       をよくお読みいただき同意いただけましたら下のボタンを押してください。
