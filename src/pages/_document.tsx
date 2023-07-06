c9ad44471310e2e4ad10c69af49ba3ea803aca1f
sa-tsuki <isobe@plusjam.jp>
2023-07-06 21:29:31 +0900

    フォントのちらつき改善とバンドルアナライザーの追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,6 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"></link>
-
         <script
           dangerouslySetInnerHTML={{
             __html: `
